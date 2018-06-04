1129eedef5db3726dac836cc39991ebe8ed8faa5
Shiftypanda <drtimddavis@gmail.com>
2018-06-05 00:29:54 +0100

    added first two buttons and flipcard function

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -10,19 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // func touchCard(_ sender   ---- are the names
+    // first one is internal name
+    // second one is exeternal name
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func touchCard(_ sender: UIButton)  {
+        flipCard(withEmoji: "👻", on: sender)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji)")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
